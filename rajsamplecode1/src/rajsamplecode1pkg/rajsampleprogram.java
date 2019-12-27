12742120115c1122d9cf09c1d219b32f4134779b
rajkapoor7 <mail2rajkapoor@gmail.com>
2019-12-27 17:55:48 +0530

    My Java Code

---
+++ rajsamplecode1/src/rajsamplecode1pkg/rajsampleprogram.java
@@ -0,0 +1,10 @@
+package rajsamplecode1pkg;
+
+public class rajsampleprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Raj sample Test Code");  
+	}
+
+}
